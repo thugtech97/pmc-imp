5f1b25d687e2078b579e9fdc8232d461912231fc
thugtech97 <juhde.1997@gmail.com>
2024-03-22 10:07:46 +0800

    quick fix again for imf controller

--- resources/views/theme/pages/customer/new-stock/edit.blade.php
+++ resources/views/theme/pages/customer/new-stock/edit.blade.php
@@ -242,8 +242,8 @@
                 .filter(([name, value]) => value !== null && value !== undefined && value.trim() !== "")
                 .map(([name, value]) => ({ name, value }));
 
-            $("#brand, #oem-id").prop('required', false);
-            $('#item-description, #uom, #usage-rate-qty, #usage-frequency, #min-qty, #max-qty, #purpose').prop('required', true);
+            $("#brand, #oem-id, #purpose").prop('required', false);
+            $('#item-description, #uom, #usage-rate-qty, #usage-frequency, #min-qty, #max-qty').prop('required', true);
             $('#stockCode').show();
             $('#add_section_only').hide();
             $('#purpose-container').hide();
