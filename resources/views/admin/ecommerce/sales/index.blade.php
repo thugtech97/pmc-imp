0845ca3f15656f5573aac90a5925b065644ff0d9
thugtech97 <juhde.1997@gmail.com>
2025-02-12 11:16:39 +0800

    commited

--- resources/views/admin/ecommerce/sales/index.blade.php
+++ resources/views/admin/ecommerce/sales/index.blade.php
@@ -122,7 +122,7 @@
                         <div class="col-12 mx-0 mb-2 p-0">
                             <form class="form-inline" id="searchForm" style="font-size: 12px;">
                                     <div class="col-2 p-0 m-0">
-                                        <input name="search" type="search" id="search" class="form-control" style="font-size:12px;width: 170px;"  placeholder="Search MRS Number" value="{{ $filter->search }}">
+                                        <input name="search" type="search" id="search" class="form-control" style="font-size:12px;width: 170px;"  placeholder="Search MRS/PA Number" value="{{ $filter->search }}">
                                     </div>
                                     <div class="col-2 p-0 m-0 row">
                                         <div class="col-2 p-0 align-self-center">
