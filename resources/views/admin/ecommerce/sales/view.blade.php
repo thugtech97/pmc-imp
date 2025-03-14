a4c8b27ffbb7af5c8a880d29174ff6671b83ce75
thugtech97 <juhde.1997@gmail.com>
2025-03-14 13:55:59 +0800

    planner remarks removed disabled

--- resources/views/admin/ecommerce/sales/view.blade.php
+++ resources/views/admin/ecommerce/sales/view.blade.php
@@ -365,7 +365,7 @@
                 <div class="form-group text-right">
                     @if ($role->name === "MCD Planner")
                         <span class="title">PLANNER REMARKS</span>
-                        <textarea id="planner_remarks" class="form-control mt-2" name="planner_remarks" placeholder="Add note..." {{ $sales->status === 'APPROVED (MCD Planner) - MRS For Verification' || $sales->status === 'Verified (MCD Verifier) - PA For MCD Manager Approval' || $sales->received_at ? 'disabled' : '' }}>{{ $sales->planner_remarks }}</textarea>
+                        <textarea id="planner_remarks" class="form-control mt-2" name="planner_remarks" placeholder="Add note...">{{ $sales->planner_remarks }}</textarea>
                         <button type="submit" class="mt-2 btn {{ ($sales->status === 'APPROVED (MCD Planner) - MRS For Verification' || $sales->status === 'VERIFIED (MCD Verifier) - MRS For MCD Manager APPROVAL') ? 'btn-success' : 'btn-success'}}" style="width: 140px; text-transform: uppercase;" {{ $sales->status === 'APPROVED (MCD Planner) - MRS For Verification' || $sales->status === 'VERIFIED (MCD Verifier) - MRS For MCD Manager APPROVAL' || $sales->received_at ? 'disabled' : '' }}>{{ $sales->status === 'APPROVED (MCD Planner) - MRS For Verification' || $sales->status === 'VERIFIED (MCD Verifier) - MRS For MCD Manager APPROVAL' || $sales->received_at ? 'SUBMITTED' : 'PROCEED'}}</button><br><br>
                         @if ($sales->received_at)
                             <button type="submit" class="btn btn-primary" style="width: 140px; text-transform: uppercase;">UPDATE</button><br><br>
