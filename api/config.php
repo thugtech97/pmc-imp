08bf2812da661e16effabae082e50017ce1de630
thugtech97 <juhde.1997@gmail.com>
2026-01-12 07:39:10 +0800

    hotfix

--- api/config.php
+++ api/config.php
@@ -1,14 +1,14 @@
 <?php 
 		
-	$serverName = "DESKTOP-HBTCDK6\SQLEXPRESS";	$connectionInfo = array( "Database"=>"PMC-WFS", "UID"=>"judeescol97", "PWD"=>"!Q2w3e!1997" );
-	//$serverName = "172.16.20.28";	$connectionInfo = array( "Database"=>"WFS", "UID"=>"sa", "PWD"=>"philsaga" );
+	//$serverName = "DESKTOP-HBTCDK6\SQLEXPRESS";	$connectionInfo = array( "Database"=>"wfs", "UID"=>"judeescol97", "PWD"=>"!Q2w3e!1997" );
+	$serverName = "172.16.20.28";	$connectionInfo = array( "Database"=>"WFS", "UID"=>"sa", "PWD"=>"philsaga" );
 	// $connectionInfo = array( "Database"=>"PMC-WFS", "UID"=>"sa", "PWD"=>"@Temp123!" );
 	$conn = sqlsrv_connect($serverName, $connectionInfo);
 	
 	
-	//$conn_d['davao']['type'] = 'sqlsrv';$conn_d['davao']['host'] = '172.16.10.42\philsaga_db';$conn_d['davao']['name'] = 'PMC-DAVAO';$conn_d['davao']['uname'] = 'app_user_read_only';$conn_d['davao']['pword'] = 'P4TGQkPz';
+	$conn_d['davao']['type'] = 'sqlsrv';$conn_d['davao']['host'] = '172.16.10.42\philsaga_db';$conn_d['davao']['name'] = 'PMC-DAVAO';$conn_d['davao']['uname'] = 'app_user_read_only';$conn_d['davao']['pword'] = 'P4TGQkPz';
 	
-	$conn_d['davao']['type'] = 'sqlsrv';$conn_d['davao']['host'] = 'DESKTOP-HBTCDK6\SQLEXPRESS';$conn_d['davao']['name'] = 'SyncHRIS';$conn_d['davao']['uname'] = 'judeescol97';$conn_d['davao']['pword'] = '!Q2w3e!1997';
+	//$conn_d['davao']['type'] = 'sqlsrv';$conn_d['davao']['host'] = 'DESKTOP-HBTCDK6\SQLEXPRESS';$conn_d['davao']['name'] = 'SyncHRIS';$conn_d['davao']['uname'] = 'judeescol97';$conn_d['davao']['pword'] = '!Q2w3e!1997';
 
 	$conn_a['agusan']['type'] = 'sqlsrv';
 	$conn_a['agusan']['host'] = '172.16.20.42\agusan_db';
--- api/hris-api-2.php
+++ api/hris-api-2.php
@@ -3,110 +3,54 @@
 include(__DIR__ . '/config.php');
 
 function get_array_emp(){
-    global $conn_d;
-    global $conn_a;
+	global $conn_d;
+	global $conn_a;
 
-    $department = $_POST["department"];
-    $array_emp = array();
+	$array_emp = array();
+	
+	$pdoempdata = new PDO($conn_a['agusan']['type'].":server=".$conn_a['agusan']['host'].";Database=".$conn_a['agusan']['name'], $conn_a['agusan']['uname'], $conn_a['agusan']['pword']);
+	$pdoempdata->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-	// Initial Query with Department Filter
-    $query_with_filter = "
-        SELECT DISTINCT e.EmpID, e.FullName, e.FName, e.MName, e.LName, d.DeptDesc, d.deptid, p.PositionDesc 
-        FROM ViewHREmpMaster e 
-        LEFT JOIN hrdepartment d ON d.deptid = e.deptid 
-        LEFT JOIN hrposition p ON p.PositionID = e.PositionID 
-        WHERE e.Active = 1 AND d.deptdesc LIKE :department
-        ORDER BY e.LName
-    ";
+	//$empdatasql = $pdoempdata->prepare("SELECT EmpID, FullName FROM ViewHREmpMaster WHERE Active = 1");
 
-	// Query without Department Filter
-	$query_without_filter = "
-		SELECT DISTINCT e.EmpID, e.FullName, e.FName, e.MName, e.LName, d.DeptDesc, d.deptid, p.PositionDesc 
+	$empdatasql = $pdoempdata->prepare("
+		SELECT e.EmpID, e.FullName, e.FName, e.MName, e.LName, d.DeptDesc, d.deptid, p.PositionDesc 
 		FROM ViewHREmpMaster e 
 		LEFT JOIN hrdepartment d ON d.deptid = e.deptid 
 		LEFT JOIN hrposition p ON p.PositionID = e.PositionID 
-		WHERE e.Active = 1
+		WHERE e.Active = 1 
 		ORDER BY e.LName
-	";
+	");
 
-/*
-    // Connect to Agusan Database
-    $pdoempdata = new PDO(
-        $conn_a['agusan']['type'] . ":server=" . $conn_a['agusan']['host'] . ";Database=" . $conn_a['agusan']['name'],
-        $conn_a['agusan']['uname'],
-        $conn_a['agusan']['pword']
-    );
-    $pdoempdata->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$empdatasql->execute();
+	$empdatasql->setFetchMode(PDO::FETCH_ASSOC);
 
-    $empdatasql = $pdoempdata->prepare($query_with_filter);
-    $empdatasql->bindValue(':department', '%' . $department . '%');
-    $empdatasql->execute();
-    // Fetch the first row to check if there are results
-    $firstRow = $empdatasql->fetch(PDO::FETCH_ASSOC);
+	for($i=0; $rowempdata = $empdatasql->fetch(); $i++){   
+		$array_emp[] = ["fullnamewithdept" => str_replace(',',' ',$rowempdata['FullName']).' : '.$rowempdata['DeptDesc'], "fullname" => $rowempdata['FName'].'*'.$rowempdata['MName'].'*'.$rowempdata['LName']];
+	}
+	
+	$pdoempdata = new PDO($conn_d['davao']['type'].":server=".$conn_d['davao']['host'].";Database=".$conn_d['davao']['name'], $conn_d['davao']['uname'], $conn_d['davao']['pword']);
+	$pdoempdata->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    if ($firstRow) {
-        $empdatasql->setFetchMode(PDO::FETCH_ASSOC);
+	$empdatasql = $pdoempdata->prepare("
+		SELECT e.EmpID, e.FullName, e.FName, e.MName, e.LName, d.DeptDesc, d.deptid, p.PositionDesc 
+		FROM ViewHREmpMaster e 
+		LEFT JOIN hrdepartment d ON d.deptid = e.deptid 
+		LEFT JOIN hrposition p ON p.PositionID = e.PositionID 
+		WHERE e.Active = 1 
+		ORDER BY e.LName
+	");
 
-        while ($rowempdata = $empdatasql->fetch()) {
-            $array_emp[] = [
-                "fullnamewithdept" => str_replace(',', ' ', $rowempdata['FullName']) . ' : ' . $rowempdata['PositionDesc'],
-                "fullname" => $rowempdata['FName'] . '*' . $rowempdata['MName'] . '*' . $rowempdata['LName']
-            ];
-        }
-    } else {
+	$empdatasql->execute();
+	$empdatasql->setFetchMode(PDO::FETCH_ASSOC);
 
-        $empdatasql = $pdoempdata->prepare($query_without_filter);
-        $empdatasql->execute();
-        $empdatasql->setFetchMode(PDO::FETCH_ASSOC);
+	for($i=0; $rowempdata = $empdatasql->fetch(); $i++){     
+		$array_emp[] = ["fullnamewithdept" => str_replace(',',' ',$rowempdata['FullName']).' : '.$rowempdata['PositionDesc'], "fullname" => $rowempdata['FName'].'*'.$rowempdata['MName'].'*'.$rowempdata['LName']];
+		//$array_emp[] .= str_replace(',',' ',$rowempdata['FullName']).' : '.$rowempdata['DeptDesc'];
+	}
 
-        while ($rowempdata = $empdatasql->fetch()) {
-            $array_emp[] = [
-                "fullnamewithdept" => str_replace(',', ' ', $rowempdata['FullName']) . ' : ' . $rowempdata['PositionDesc'],
-                "fullname" => $rowempdata['FName'] . '*' . $rowempdata['MName'] . '*' . $rowempdata['LName']
-            ];
-        }
-    }
-*/
-    // Connect to Davao Database
-    $pdoempdata = new PDO(
-        $conn_d['davao']['type'] . ":server=" . $conn_d['davao']['host'] . ";Database=" . $conn_d['davao']['name'],
-        $conn_d['davao']['uname'],
-        $conn_d['davao']['pword']
-    );
-    $pdoempdata->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    $empdatasql = $pdoempdata->prepare($query_without_filter);
-    //$empdatasql->bindValue(':department', '%' . $department . '%');
-    $empdatasql->execute();
-    $firstRow = $empdatasql->fetch(PDO::FETCH_ASSOC);
-
-    if ($firstRow) {
-        $empdatasql->setFetchMode(PDO::FETCH_ASSOC);
-
-        while ($rowempdata = $empdatasql->fetch()) {
-            $array_emp[] = [
-                "fullnamewithdept" => str_replace(',', ' ', $rowempdata['FullName']) . ' : ' . $rowempdata['PositionDesc'],
-                "fullname" => $rowempdata['FName'] . '*' . $rowempdata['MName'] . '*' . $rowempdata['LName']
-            ];
-        }
-    } else {
-        // Query without Department Filter
-        $empdatasql = $pdoempdata->prepare($query_without_filter);
-        $empdatasql->execute();
-        $empdatasql->setFetchMode(PDO::FETCH_ASSOC);
-
-        while ($rowempdata = $empdatasql->fetch()) {
-            $array_emp[] = [
-                "fullnamewithdept" => str_replace(',', ' ', $rowempdata['FullName']) . ' : ' . $rowempdata['PositionDesc'],
-                "fullname" => $rowempdata['FName'] . '*' . $rowempdata['MName'] . '*' . $rowempdata['LName']
-            ];
-        }
-    }
-
-    // Return the results as JSON
-    echo json_encode($array_emp);
+	echo json_encode($array_emp);
 }
-
 
 function get_requestor(){
 	global $conn_d;
